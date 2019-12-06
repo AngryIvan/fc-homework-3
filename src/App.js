a96d5b830f27678ae141f0777f153bd130d741fa
Ivan Delendick <Ivan_Delendick@epam.com>
2019-12-06 15:34:45 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const clickMe = () => {
+    alert(22)
+  }
+
   return (
     <>
-      <h1>Homework</h1>
+      <h1 onMouseEnter={clickMe}>Homework</h1>
     </>
+  //   <Switch>
+  //   <Route exact path="/" component={HomePage} />
+  //   <Route path={`/episodes/:id`} component={EpisodePage} />
+  //   <Route component={NotFoundPage} />
+  // </Switch>
   );
 }
 
