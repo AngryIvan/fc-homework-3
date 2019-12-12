5a388aef6982922ec719611e408261866a5a4411
Ivan Delendick <Ivan_Delendick@epam.com>
2019-12-12 10:54:08 +0300

    add error boundary

--- src/components/Film/FilmHeader.js
+++ src/components/Film/FilmHeader.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Film.module.css'
-import { FilmCover } from '../film/FilmCover';
-import { FilmTitle } from '../film/FilmTitle';
-import { FilmRating } from '../film/FilmRating';
-import { FilmInfo } from '../film/FilmInfo';
-import { FilmDescription } from '../film/FilmDescription';
+import { FilmCover } from './FilmCover';
+import { FilmTitle } from './FilmTitle';
+import { FilmRating } from './FilmRating';
+import { FilmInfo } from './FilmInfo';
+import { FilmDescription } from './FilmDescription';
 
 export const FilmHeader = (props) => {
   return (
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,17 +1,20 @@
 import React from 'react';
-// import styles from './MainPage.module.css'
 import { Search } from '../search/Search';
 import { Results } from '../results/Results';
 import { Logo } from '../utils/Logo';
+import ErrorBoundary from '../utils/ErrorBoundary';
 
 export class MainPage extends React.Component {
   render() {
     return (
      <>
-      <header>
+     <ErrorBoundary>      
+       <header>
         <Logo></Logo>
         <Search></Search>
       </header>
+    </ErrorBoundary>
+
       <Results></Results>
       <footer>
         <Logo></Logo>
