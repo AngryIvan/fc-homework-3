31648ed96764387c5701b336aa15217951da7a07
AngryIvan <leftmetalban@gmail.com>
2020-01-03 12:11:12 +0300

    fix search filter

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -4,19 +4,18 @@ import styles from './Search.module.css'
 import SearchInput from './SearchInput';
 import SearchButton from './SearchButton';
 import SearchFilter from './SearchFilter';
-import { switchSearchBy } from '../../store/actions'
 
 class Search extends React.Component {
   render() {
     return (
-     <div className={styles.searchContainer}>
-      <h1>Find your movie</h1>
-      <section className={styles.searchForm}>
-       <SearchInput className={styles.searchInput} />
-       <SearchButton className={styles.searchButton} />
-      </section>
-      <SearchFilter switch1={switchSearchBy} />
-     </div>
+      <div className={styles.searchContainer}>
+        <h1>Find your movie</h1>
+        <section className={styles.searchForm}>
+          <SearchInput className={styles.searchInput} />
+          <SearchButton className={styles.searchButton} />
+        </section>
+        <SearchFilter />
+      </div>
     );
   }
 }
@@ -25,8 +24,4 @@ const mapStateToProps = (state) => ({
   films: state,
 })
 
-const mapDispatchToProps = {
-  switchSearchBy
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Search);
+export default connect(mapStateToProps, null)(Search);
